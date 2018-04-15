6a84b65cbd046eb4615d1d6346443546a879040c
Will Payne <w.payne.fc@gmail.com>
2018-04-14 19:00:11 -0500

    Configure redis subscription functionality

--- app.rb
+++ app.rb
@@ -9,18 +9,23 @@ end
 $redis = Redis.new( url: "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}" )
 
 class App < Sinatra::Application
-  get '/things' do
-    things = $redis.hvals('things')
-    status 200
-    things.to_json
-  end
+  # get '/things' do
+  #   things = $redis.hvals('things')
+  #   status 200
+  #   things.to_json
+  # end
+
+  # loop do
+  #   sleep 10
+  #   Worker.perform_async(req['id'])
+  # end
   
-  post '/things' do
+  post '/' do
     content_type :json
     req = JSON.load(request.body.read.to_s)
-    $redis.hset('things', req['id'], req['thing'])
+    # $redis.hset('jobs', req['id'], req['hash'])
     Worker.perform_async(req['id'])
-    "enqueue #{req['id']}, #{req['thing']}"
+    # "enqueue #{req['id']}, #{req['thing']}"
     status 202
   end
 end
--- lib/worker.rb
+++ lib/worker.rb
@@ -1,14 +1,41 @@
 require 'sidekiq'
+require 'digest'
+require 'net/http'
+require 'uri'
+require 'json'
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}" }
+  config.redis = { url: "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}", timeout: 0 }
 end
 
 class Worker
   include Sidekiq::Worker
   def perform(id)
-    thing = $redis.hget('things', id)
-    sleep 10
-    p "The work is done: #{thing}"
+    # ipfs_uri = $redis.hget('job', id) #Assigns a string to job variable that is the IPFS hash of the document location.
+    # uri = URI.parse("http://encoder:4500") #TODO: Add in environmental variable management for encoder URI
+
+    $redis.subscribe('jobs') do |on|
+  	  on.message do |msg|
+        data = JSON.parse(msg)
+        # puts "##{channel} - [#{data['user']}]: #{data['msg']}"
+        header = {'Content-Type': 'text/json'}
+
+        # Get SHA256 Hash of a file
+		hashed_ipfs_uri = Digest::SHA256.hexdigest msg.ipfs_uri #Hash the URI using SHA256
+
+		hashed_data = {document_id: msg.id, document_hash: hashed_ipfs_uri}
+
+
+		# Create the HTTP objects
+		http = Net::HTTP.new(uri.host, uri.port)
+		request = Net::HTTP::Post.new(uri.request_uri, header)
+		request.body = hashed_data.to_json
+
+
+		# Send the request
+		response = http.request(request) # TODO: Add in try/catch and error handling in case something happens here
+  	  end
+	end
+
   end
 end
